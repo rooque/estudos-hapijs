83ea93b89a234d136b5986b4db09b9725a42ec30
Victor Roque <victor.rooque@gmail.com>
2018-02-23 15:05:07 -0300

    fix good logging

--- src/plugins/good.js
+++ src/plugins/good.js
@@ -1,30 +1,21 @@
-const goodOptions = {
+const options = {
   ops: {
-    interval: 1000
+      interval: 1000
   },
   reporters: {
-    myConsoleReporter: [
-      {
-        module: "good-squeeze",
-        name: "Squeeze",
-        args: [
-          {
-            log: "*",
-            response: "*"
-          }
-        ]
-      },
-      {
-        module: "good-console"
-      },
-      "stdout"
-    ]
+      console: [{
+          module: 'good-squeeze',
+          name: 'Squeeze',
+          args: [{ log: '*', response: '*' }]
+      }, {
+          module: 'good-console'
+      }, 'stdout']
   }
 };
 
 export default async server => {
   await server.register({
     plugin: require("good"),
-    goodOptions
+    options
   });
 };
